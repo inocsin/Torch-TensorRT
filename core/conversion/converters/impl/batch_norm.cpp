ed84baa34b74d3c6c6a6b19f61b3d31627124f42
inocsin <vcheungyi@163.com>
2021-03-01 10:24:55 +0800

    add layer_norm plugin
    Signed-off-by: inocsin <vcheungyi@163.com>

--- core/conversion/converters/impl/batch_norm.cpp
+++ core/conversion/converters/impl/batch_norm.cpp
@@ -1,6 +1,7 @@
 #include "core/conversion/converters/converters.h"
 #include "core/util/prelude.h"
 #include "torch/torch.h"
+#include "plugins/layer_norm_plugin.h"
 
 namespace trtorch {
 namespace core {
@@ -74,6 +75,93 @@ auto batch_norm_registrations TRTORCH_UNUSED = RegisterNodeConversionPatterns().
       }
 
       ctx->AssociateValueAndTensor(n->outputs()[0], out_tensor);
+      return true;
+    }}).pattern({R"SIG(aten::layer_norm(Tensor input, int[] normalized_shape,
+                Tensor? weight=None, Tensor? bias=None, 
+                float eps=1.0e-05, bool cudnn_enable=True) -> (Tensor))SIG",
+    [](ConversionCtx* ctx, const torch::jit::Node* n, args& args) -> bool {
+      auto input = args[0].ITensor(); // assumes non-static input Tensor
+      auto orig_shape = input->getDimensions();
+      auto shape = util::toVec(orig_shape);
+      auto tensor_type = util::toATenDType(input->getType());
+      auto options = torch::TensorOptions().dtype(tensor_type);
+
+      auto normalized_shape = args[1].unwrapToIntList().vec();
+      torch::Tensor weight, bias;
+      //args[1].isIValue()
+      
+      auto eps = args[4].unwrapToDouble(1e-5f);
+
+      if (ctx->input_is_dynamic) {
+        if (args[2].isIValue()) {
+          weight = args[2].unwrapToTensor();
+        }
+        if (args[3].isIValue()) {
+          bias = args[3].unwrapToTensor();
+        }
+        
+      } else {
+        if (args[2].isIValue()) {
+          weight = args[2].unwrapToTensor(at::full({normalized_shape}, 1, {options}));
+        }
+        if (args[3].isIValue()) {
+          bias = args[3].unwrapToTensor(at::full({normalized_shape}, 0, {options}));
+        }
+        
+      }
+
+      // LOG_DEBUG("momentum disregarded");
+      // LOG_DEBUG("training disregarded");
+      // LOG_DEBUG("cudnn disregarded");
+
+      auto should_unpack = util::toVec(orig_shape).size() < 4;
+    
+      if (should_unpack) {
+        // expand spatial dims from 1D to 2D
+        auto new_shape = util::toDimsPad(util::toVec(orig_shape), 4);
+        LOG_DEBUG(
+            "Input shape is less than 4D got: "
+            << orig_shape << ", inserting shuffle layer to reshape to 4D tensor shape: " << new_shape);
+        auto in_shuffle = ctx->net->addShuffle(*input);
+        in_shuffle->setReshapeDimensions(new_shape);
+        in_shuffle->setName(std::string("[Reshape input to " + util::toStr(new_shape) + ']').c_str());
+        input = in_shuffle->getOutput(0);
+      }
+
+      auto creator = new plugins::LayerNormPluginCreator();
+                 auto plugin = creator->createPlugin(
+                     "layer_norm",
+                     normalized_shape,
+                     weight,
+                     bias,
+                     eps);
+
+                 auto layer_norm =
+                     ctx->net->addPluginV2(reinterpret_cast<nvinfer1::ITensor* const*>(&input), 1, *plugin);
+                 TRTORCH_CHECK(layer_norm, "Unable to create layer_norm plugin from node" << *n);
+
+                 layer_norm->setName(util::node_info(n).c_str());
+      // auto scale = gamma / torch::sqrt(var + eps);
+      // auto bias = beta - mean * scale;
+
+      // auto scale_weights = Weights(ctx, scale);
+      // auto bias_weights = Weights(ctx, bias);
+
+      // auto power = Weights(ctx, at::ones_like(scale));
+      // auto bn = ctx->net->addScaleNd(
+      //     *input, nvinfer1::ScaleMode::kCHANNEL, bias_weights.data, scale_weights.data, power.data, 1);
+      // bn->setName(util::node_info(n).c_str());
+      // auto out_tensor = bn->getOutput(0);
+
+      // if (should_unpack) {
+      //   LOG_DEBUG("Inserting shuffle layer to reshape to back to original shape: " << orig_shape);
+      //   auto out_shuffle = ctx->net->addShuffle(*out_tensor);
+      //   out_shuffle->setReshapeDimensions(orig_shape);
+      //   out_shuffle->setName(std::string("[Reshape output to " + util::toStr(orig_shape) + ']').c_str());
+      //   out_tensor = out_shuffle->getOutput(0);
+      // }
+
+      ctx->AssociateValueAndTensor(n->outputs()[0], layer_norm->getOutput(0));
       return true;
     }});
 
